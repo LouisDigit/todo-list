c20ee3fe1d65fb39a2d1d32120e73c1fff7c6499
Louis <louis.vanoise@epitech.eu>
2023-01-13 12:03:25 +0100

    Advance setup project and packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import "./App.css";
+import "@/App.css";
+import { Home } from "@/pages/Home";
+import { Login } from "@/pages/Login";
+import { Register } from "@/pages/Register";
+import { Error } from "@/_utils/Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 class="">Test</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}></Route>
+
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
